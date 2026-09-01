9d7a44f66d95b501c4f2c479bcb0f39b773adf35
Areg472 <aregazatyank@outlook.com>
2026-09-01 11:44:12 +0400

    updated meta

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Highlands YSWS!",
-  description: "A mountain themed You Ship We Ship!",
+  description:
+    "Highlands is a mountain themed You Ship We Ship where you code or do hardware, climb digital mountains and earn prizes such as hiking gear, earphones, 3d printers, and more + a chance to get invited to an IRL hackathon in Armenia.\n",
 };
 
 export const viewport = {
